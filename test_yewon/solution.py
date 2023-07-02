1ceb46b46e24780247acf37eb349b8f8e4f6c6f3
eyewon <111197714+eyewon@users.noreply.github.com>
2023-07-02 23:10:20 +0900

    yewon

---
+++ test_yewon/solution.py
@@ -0,0 +1,9 @@
+def solution(targets):
+    targets.sort(key=lambda x:x[1])
+    answer = 0
+    end=-1
+    for s,e in targets:
+        if end <= s:
+            answer+=1
+            end=e
+    return answer
