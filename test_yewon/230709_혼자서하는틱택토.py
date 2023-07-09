af934ee1bfdc7770ee03a7e7f5502af345b95a9b
eyewon <111197714+eyewon@users.noreply.github.com>
2023-07-09 23:57:56 +0900

    Create 230709_혼자서하는틱택토.py

---
+++ test_yewon/230709_혼자서하는틱택토.py
@@ -0,0 +1,28 @@
+def solution(board):
+    cnt_o, cnt_x=sum([arr.count('O') for arr in board]), sum([arr.count('X') for arr in board])
+    
+    if 0 <= cnt_o-cnt_x <=1:
+        rb=[board[0][i]+board[1][i]+board[2][i] for i in range(3)]
+        
+        winO, winX = 0,0
+        
+        for i,j in zip(board,rb):
+            if "OOO" in [i,j]: winO+=1
+            if "XXX" in [i.j]: winX+=1
+            
+        diagonal = [board[0][0]+board[1][1]+board[2][2], board[0][2]+board[1][1]+board[2][0]]
+        
+        winO+= diagonal.count('OOO')
+        winO+= diagonal.count('XXX')
+        
+        if winX and winO: 
+            return 0
+        if winO and winX==0 and cnt_o == cnt_x:
+            return 0
+        if winX and winO==0 and cnt_o != cnt_x:
+            return 0
+        
+        return 1
+    
+    
+    return 0
