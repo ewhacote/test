debcc4780c01cfc55d0e5b1c6faaed80dac9e2d0
Cherry <79985974+chaeri93@users.noreply.github.com>
2023-07-09 23:53:50 +0900

    2023-07-09 혼자서 하는 틱택토

---
+++ test_cherry/20230709_혼자서 하는 틱택토.py
@@ -0,0 +1,21 @@
+def solution(board):
+    o, x = sum([arr.count('O') for arr in board]), sum([arr.count('X') for arr in board])
+    if 0 <= o - x <= 1:
+        rb = [board[0][i] + board[1][i] + board[2][i] for i in range(3)]
+        winO, winX = 0, 0
+        for i, j in zip(board, rb):
+            if 'OOO' in [i, j]:
+                winO += 1
+            if 'XXX' in [i, j]:
+                winX += 1
+
+        diagnol = [board[0][0] + board[1][1] + board[2][2], board[0][2] + board[1][1] + board[2][0]]
+        winO += diagnol.count('OOO')
+        winX += diagnol.count('XXX')
+
+        if winO and winX: return 0
+        if winO and winX == 0 and o == x: return 0
+        if winX and winO == 0 and o != x: return 0
+
+        return 1
+    return 0
